0457ebdb4aa7a376b930e3ef2a6fded35e04f436
Emanuel de Souza <emanuelcds@gmail.com>
2016-05-24 11:34:12 -0300

    feat(*): Added choice widget to django admin form and removed unecessary byte literal notation from initial migration once its giving problem to generate new migrations in python 3+.

--- rest_hooks/admin.py
+++ rest_hooks/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-
+from django.conf import settings
+from django import forms
 from rest_hooks.models import Hook
+
+
+HOOK_EVENTS = getattr(settings, 'HOOK_EVENTS', None)
+if HOOK_EVENTS is None:
+    raise Exception("You need to define settings.HOOK_EVENTS!")
+
+
+class HookForm(forms.ModelForm):
+    ADMIN_EVENTS = [(x, x) for x in HOOK_EVENTS.keys()]
+
+    def __init__(self, *args, **kwargs):
+        super(HookForm, self).__init__(*args, **kwargs)
+        self.fields['event'] = forms.ChoiceField(choices=self.ADMIN_EVENTS)
 
 
 class HookAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Hook._meta.fields]
-    raw_id_fields = ['user',]
+    raw_id_fields = ['user', ]
+    form = HookForm
+
 admin.site.register(Hook, HookAdmin)
